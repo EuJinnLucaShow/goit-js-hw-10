8cd332f3f5fd74f491b9cc53efb7fc2c6cb34c97
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-04 08:38:25 +0200

    Update fetchCountries.js
    Added all error responses
    Client error responses (400 – 499)
    Server error responses (500 – 599)

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://restcountries.com/v3.1/name';
 
 const fetchCountries = async name => {
   const response = await fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`);  
-  if (response.status >= 400 && response.status <= 499) {    
+  if (response.status >= 400 && response.status <= 599) {    
     return Promise.reject(new Error());    
   }
   return await response.json();
